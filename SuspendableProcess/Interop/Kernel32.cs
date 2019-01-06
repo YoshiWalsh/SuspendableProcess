de496310256c36f0d984c0a15aae29b43300c119
Joshua Walsh <josh@ymindustries.com>
2019-01-06 23:22:53 +1100

    Remove unnecessary code

--- SuspendableProcess/Interop/Kernel32.cs
+++ SuspendableProcess/Interop/Kernel32.cs
@@ -5,119 +5,14 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-#pragma warning disable SA1121 // Use built-in type alias
-#pragma warning disable SA1307 // Accessible fields must begin with upper-case letter
 namespace SuspendableProcess.Interop
 {
-    // This file contains code from https://www.codeproject.com/Articles/230005/%2FArticles%2F230005%2FLaunch-a-process-suspended which is used in accordance with the CPOLicense
     // This file also contains code from https://www.pinvoke.net/index.aspx. The license for this is unknown, but the PInvoke.net homepage encourages copy/pasting.
     public class Kernel32
     {
-        private const uint STARTF_USESHOWWINDOW = 0x00000001;
-        private const uint STARTF_USESTDHANDLES = 0x00000100;
-        private const uint STARTF_FORCEONFEEDBACK = 0x00000040;
-        private const uint NORMAL_PRIORITY_CLASS = 0x00000020;
-        private const uint CREATE_BREAKAWAY_FROM_JOB = 0x01000000;
-        private const uint CREATE_NO_WINDOW = 0x08000000;
-        private const uint CREATE_UNICODE_ENVIRONMENT = 0x00000400;
-        private const short SW_SHOW = 5;
-        private const short SW_HIDE = 0;
-        private const int STD_OUTPUT_HANDLE = -11;
-        private const int HANDLE_FLAG_INHERIT = 1;
-        private const uint GENERIC_READ = 0x80000000;
-        private const uint FILE_ATTRIBUTE_READONLY = 0x00000001;
-        private const uint FILE_ATTRIBUTE_NORMAL = 0x00000080;
-        private const int OPEN_EXISTING = 3;
-        private const uint CREATE_NEW_CONSOLE = 0x00000010;
-        private const uint STILL_ACTIVE = 0x00000103;
-
         private const int MAX_DEFAULTCHAR = 2;
         private const int MAX_LEADBYTES = 12;
         private const int MAX_PATH = 260;
-
-        [Flags]
-        public enum ProcessCreationFlags : uint
-        {
-            ZERO_FLAG = 0x00000000,
-            CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
-            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
-            CREATE_NEW_CONSOLE = 0x00000010,
-            CREATE_NEW_PROCESS_GROUP = 0x00000200,
-            CREATE_NO_WINDOW = 0x08000000,
-            CREATE_PROTECTED_PROCESS = 0x00040000,
-            CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
-            CREATE_SEPARATE_WOW_VDM = 0x00001000,
-            CREATE_SHARED_WOW_VDM = 0x00001000,
-            CREATE_SUSPENDED = 0x00000004,
-            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
-            DEBUG_ONLY_THIS_PROCESS = 0x00000002,
-            DEBUG_PROCESS = 0x00000001,
-            DETACHED_PROCESS = 0x00000008,
-            EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
-            INHERIT_PARENT_AFFINITY = 0x00010000
-        }
-
-        [DllImport("kernel32.dll")]
-        public static extern bool CreateProcess(
-            string lpApplicationName,
-            string lpCommandLine,
-            IntPtr lpProcessAttributes,
-            IntPtr lpThreadAttributes,
-            bool bInheritHandles,
-            ProcessCreationFlags dwCreationFlags,
-            IntPtr lpEnvironment,
-            string lpCurrentDirectory,
-            ref STARTUPINFO lpStartupInfo,
-            out PROCESS_INFORMATION lpProcessInformation);
-
-        [DllImport("kernel32.dll")]
-        public static extern uint ResumeThread(IntPtr hThread);
-
-        [DllImport("kernel32.dll")]
-        public static extern uint SuspendThread(IntPtr hThread);
-
-        [DllImport("kernel32.dll")]
-        public static extern int CloseHandle(int hObject);
-        [DllImport("kernel32.dll")]
-        public static extern int WaitForSingleObject(IntPtr hHandle, int dwMilliseconds);
-        [DllImport("kernel32.dll")]
-        public static extern int GetExitCodeProcess(int hProcess, ref int lpExitCode);
-        [DllImport("kernel32.dll")]
-        public static extern bool CreatePipe(out IntPtr phReadPipe, out IntPtr phWritePipe, IntPtr lpPipeAttributes, uint nSize);
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern unsafe bool ReadFile(
-            IntPtr hfile,
-            void* pBuffer,
-            int numberOfBytesToRead,
-            int* pNumberOfBytesRead,
-            int pOverlapped);
-
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr GetStdHandle(int stdHandle);
-        [DllImport("kernel32.dll")]
-        public static extern bool SetStdHandle(int nStdHandle, IntPtr hHandle);
-        [DllImport("kernel32.dll")]
-        public static extern bool SetHandleInformation(IntPtr hObject, int dwMask, uint dwFlags);
-        [DllImport("kernel32", SetLastError = true)]
-        public static extern IntPtr CreateFile(
-            string filename,
-            uint desiredAccess,
-            uint shareMode,
-            IntPtr attributes,
-            uint creationDisposition,
-            uint flagsAndAttributes,
-            IntPtr templateFile);
-
-        public static unsafe int Read(byte[] buffer, int index, int count, IntPtr hStdOut)
-        {
-            int n = 0;
-            fixed (byte* p = buffer)
-            {
-                if (!ReadFile(hStdOut, p + index, count, &n, 0))
-                    return 0;
-            }
-            return n;
-        }
 
         /// <summary>
         /// Gets information on a named code page.
@@ -166,36 +61,6 @@ namespace SuspendableProcess.Interop
             return count;
         }
 
-        public struct STARTUPINFO
-        {
-            public uint cb;
-            public string lpReserved;
-            public string lpDesktop;
-            public string lpTitle;
-            public uint dwX;
-            public uint dwY;
-            public uint dwXSize;
-            public uint dwYSize;
-            public uint dwXCountChars;
-            public uint dwYCountChars;
-            public uint dwFillAttribute;
-            public uint dwFlags;
-            public short wShowWindow;
-            public short cbReserved2;
-            public IntPtr lpReserved2;
-            public IntPtr hStdInput;
-            public IntPtr hStdOutput;
-            public IntPtr hStdError;
-        }
-
-        public struct PROCESS_INFORMATION
-        {
-            public IntPtr hProcess;
-            public IntPtr hThread;
-            public uint dwProcessId;
-            public uint dwThreadId;
-        }
-
         [StructLayout(LayoutKind.Sequential)]
         public struct CPINFOEX
         {
@@ -218,5 +83,3 @@ namespace SuspendableProcess.Interop
         }
     }
 }
-#pragma warning restore SA1121 // Use built-in type alias
-#pragma warning restore SA1307 // Accessible fields must begin with upper-case letter
